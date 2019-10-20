e972815d54e6f844684415efecfdc92ad24da909
DIP2202 <dip22febsaha@gmail.com>
2019-10-20 16:14:49 +0530

    Initial commit

---
+++ app/src/main/java/com/londonappbrewery/climapm/ChangeCityController.java
@@ -0,0 +1,42 @@
+package com.londonappbrewery.climapm;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class ChangeCityController extends AppCompatActivity {
+    EditText edtChangeCity;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_city_layout);
+
+        edtChangeCity = findViewById(R.id.queryET);
+        ImageButton backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        edtChangeCity.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                Intent data = new Intent();
+                data.putExtra("City", edtChangeCity.getText().toString());
+                setResult(RESULT_OK, data);
+                finish();
+                return true;
+            }
+        });
+    }
+}
